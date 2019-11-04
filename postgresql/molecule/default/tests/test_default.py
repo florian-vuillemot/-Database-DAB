6784555bbe890c5ca18100d2cbb884ced8c3c533
vuille_f <florian.vuillemot@posteo.net>
2019-11-04 16:54:39 +0100

    Create test for postgresql install

--- postgresql/molecule/default/tests/test_default.py
+++ postgresql/molecule/default/tests/test_default.py
@@ -1,15 +1,36 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
+
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('pkg', [
+  'postgresql11-server',
+])
+def test_is_installed(host, pkg):
+    package = host.package(pkg)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('svc', [
+  'postgresql-11'
+])
+def test_is_enabled(host, svc):
+    service = host.service(svc)
+
+    assert service.is_enabled
+
+
+@pytest.mark.parametrize('svc', [
+    'postgresql-11'
+])
+def test_is_running(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
