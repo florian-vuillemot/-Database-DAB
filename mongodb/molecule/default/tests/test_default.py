4260463fd43d167a078414e342f632bf07982604
vuille_f <florian.vuillemot@posteo.net>
2019-11-17 19:31:47 +0100

    Add test on MongoDB version

--- mongodb/molecule/default/tests/test_default.py
+++ mongodb/molecule/default/tests/test_default.py
@@ -24,3 +24,9 @@ def test_is_running(host, svc):
     service = host.service(svc)
 
     assert service.is_running
+
+
+def test_nginx_is_installed(host):
+    mongodb = host.package('mongodb-org')
+
+    assert mongodb.version.startswith('4.2')
