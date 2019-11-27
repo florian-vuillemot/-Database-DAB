af5697a682d53b588435fac2002ab48c4de7c8d1
vuille_f <florian.vuillemot@posteo.net>
2019-11-27 22:36:19 +0100

    Update test

--- postgresql/molecule/default/tests/test_default.py
+++ postgresql/molecule/default/tests/test_default.py
@@ -2,6 +2,7 @@ import os
 import pytest
 
 import testinfra.utils.ansible_runner
+import psycopg2
 
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -34,3 +35,16 @@ def test_is_running(host, svc):
     service = host.service(svc)
 
     assert service.is_running
+
+
+def test_then_databases_are_created(host):
+    db_default = ['postgres', 'template0', 'template1']
+    _host = host.interface("eth0").addresses[0]
+    c_str = "dbname='test_db' user='foo' host='" + _host + "' password='bar'"
+    conn = psycopg2.connect(c_str)
+    cur = conn.cursor()
+    cur.execute("""SELECT datname from pg_database""")
+    rows = cur.fetchall()
+    to_create = set(['test_db', 'test_db2', *db_default])
+    created = set([r[0] for r in rows])
+    assert not (to_create ^ created)
